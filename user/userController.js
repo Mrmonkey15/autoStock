8e31ec3838a16efb4a8566327a271632d1868bf0
Mrmonkey15 <lucas.network15@gmail.com>
2024-11-28 16:18:02 -0300

    create

--- user/userController.js
+++ user/userController.js
@@ -7,32 +7,41 @@ router.get("/admin/create-user",(req,res)=>{
     res.render('admin/create')
 })
 
-router.post("/api/register/user", async(req,res)=>{
-    try{
+router.post("/api/register/user", async (req, res) => {
+    try {
         const name = req.body.name;
         const username = req.body.username;
         const email = req.body.email;
-        const password = req.body.password
-        const role = req.body.role; 
-    
-        const salt = bcrypt.genSalt(10)
-        const hash = bcrypt.hash(password,salt)
-    
-       const response =await Users.create({
-            username:username,
-            email:email,
-            password:hash,
-            role:role
-        })
+        const password = req.body.password;
+        const role = req.body.role;
 
-        res.send(response.toJSON)
-    }catch(err){
-        console.log("erro ao cadastrar usuário")
+        // Verificar se o e-mail já está cadastrado
+        const emailSearch = await Users.findOne({
+            where: { email }
+        });
+
+        if (!emailSearch) {
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+
+            const response = await Users.create({
+                name: name,
+                username: username,
+                email: email,
+                password: hash,
+                role: role
+            });
+
+            res.render("admin/create", { successMessage: "Usuário cadastrado com sucesso!" });
+        } else {
+            // E-mail já está cadastrado
+            res.render("admin/create", { errorMessage: "E-mail já cadastrado!" });
+        }
+    } catch (err) {
+        console.log("Erro ao cadastrar usuário", err);
+        res.render("admin/create", { errorMessage: "Erro ao cadastrar usuário. Tente novamente." });
     }
-
-    
-
-})
+});
 
 
 
